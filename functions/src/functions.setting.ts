b90541fe679165e502300480c29cc068abf45912
lmk0420 <lmk19960420@gmail.com>
2020-10-26 00:28:29 +0900

    schedulerを設定

--- functions/src/functions.setting.ts
+++ functions/src/functions.setting.ts
@@ -1,7 +1,11 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
+export const REGION = 'asia-northeast1'
 export const config = functions.config()
 
 admin.initializeApp()
 export const firestore = admin.firestore()
+firestore.settings({
+  timestampsInSnapshots: true
+})
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
 import * as functions from 'firebase-functions'
+import { REGION } from './functions.setting'
 import { expressReceiver } from './slack/app'
 import { sendConnpassInfoByWebhook } from './service/slack/slackService'
 
-export const sendConnpassDataToSlack = functions.https.onRequest(async (request, response) => {
+export const sendConnpassDataToSlack = functions.region(REGION).pubsub
+.schedule('55 8 * * *').timeZone('Asia/Tokyo').onRun(async context => {
   await sendConnpassInfoByWebhook()
-  response.send('OK')
 })
 
 export const slack = functions.https.onRequest(expressReceiver.app)
