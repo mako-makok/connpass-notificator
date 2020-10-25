41ce97a847e31b19dd32ecf64be20b6963882d75
lmk0420 <lmk19960420@gmail.com>
2020-10-25 23:42:47 +0900

    functionsをawaitするように修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,14 @@ import { expressReceiver } from './slack/app'
 import { sendConnpassInfoByWebhook } from './service/slack/slackService'
 
 export const sendConnpassDataToSlack = functions.https.onRequest(async (request, response) => {
-  sendConnpassInfoByWebhook()
+  await sendConnpassInfoByWebhook()
   response.send('OK')
 })
 
 export const slack = functions.https.onRequest(expressReceiver.app)
 
 export const testData = functions.https.onRequest(async (request, response) => {
-  firestore.collection('users').doc().set({
+  await firestore.collection('users').doc().set({
     slackId: 'Makoto',
     hasConnpass: true,
     connpassParams: [
