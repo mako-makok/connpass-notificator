db5b3cde416b92e677463c2c73f881000beedc7c
lmk0420 <lmk19960420@gmail.com>
2020-11-14 23:59:17 +0900

    import resolverの設定

---
+++ functions/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        'prettier',
+        'prettier/@typescript-eslint'
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    'import/resolver': {
+        webpack: { config: path.join(__dirname, '/config/webpack.config.js') },
+    },
+};
